e40b1f0cdee24d04b31e7283cfeee16a4b135152
Sudhir <sudhirmunna007@gmail.com>
2020-04-09 11:35:56 -0500

    header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'login-app';
+
+  loginForm: FormGroup;
+  @Output() emitLoginEvent = new EventEmitter<any>();
+  // @Output() open: EventEmitter<any> = new EventEmitter();
+
+  constructor() {
+   }
+
+  ngOnInit(): void {
+
+    this.loginForm = new FormGroup({
+      userName: new FormControl(''),
+      password: new FormControl(''),
+    });
+
+  }
+
+  onSubmit() {
+    console.warn(this.loginForm.value);
+    localStorage.setItem('profile', JSON.stringify(this.loginForm.value));
+    this.emitLoginEvent.emit(this.loginForm.value);
+
+  }
+
+
 }
